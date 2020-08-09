0845aa5f6fbaa33f2aa163b4fcf853acd263c3c1
wdynx <56562153+wdynx@users.noreply.github.com>
2020-08-09 22:50:24 +0800

    Update 057.py

--- 057.py
+++ 057.py
@@ -7,3 +7,24 @@ while True:
         print(num1+num2)
     except:
         break
+
+# 常规做法
+while True:
+    try:
+        a = input().strip()
+        b = input().strip()
+        if len(a) < len(b):
+            a, b = b, a
+        a = [i for i in a]
+        b = [i for i in b]
+        flag = 0
+        res = []
+        for i in range(len(a)):
+            num = flag + int(a[-1-i]) + (int(b[-1-i]) if i < len(b) else 0)
+            flag, num = divmod(num, 10)
+            res.append(str(num))
+        if flag == 1:
+            res.append('1')
+        print(''.join(res[::-1]))
+    except:
+        break
